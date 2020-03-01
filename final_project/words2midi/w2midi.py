80766b7373768fdc8bdcb0b3ff967ac789100686
eNigma <miguel_on_94@hotmail.com>
2020-03-02 00:14:16 +0100

    embeddings to midi mapping added

---
+++ final_project/words2midi/w2midi.py
@@ -0,0 +1,44 @@
+import numpy as np
+from gensim.models import KeyedVectors
+from itertools import repeat
+
+
+def find_range(point, range_limits):
+    n_limits = len(range_limits)
+    for limit in range(n_limits - 1):
+        if (point > range_limits[limit]) and (point < range_limits[limit + 1]):
+            return limit
+    raise ValueError
+
+
+def colapse_into_10(space_50d):
+    ndim = space_50d.ndim
+    if ndim == 1:  # If a word embedding
+        space_10d = np.zeros(10)
+        for idx in range(10):
+            space_10d[idx] = np.sum(space_50d[idx*5:(idx+1)*5])
+    else:
+        space_10d = np.zeros([space_50d.shape[0], 10])
+        for idx in range(10):
+            space_10d[idx] = np.sum(space_50d[idx*5:(idx+1)*5],)
+    return space_10d
+
+
+def embspace_to_midi(word_embedding, n_words):
+    """
+    word_embedding: The 50dim vector resulting of difference between multiple words embedding
+    n_words: Number of words used to create the word_embedding
+    """
+    embedding = np.load('mappings.npy')
+    reduced_10 = colapse_into_10(embedding)
+    maxs = np.max(reduced_10, axis=0)*n_words
+    mins = np.min(reduced_10, axis=0)*n_words
+    steps = (maxs - mins) / 129
+    mappings = np.array(list((map(np.arange, mins, maxs, steps))))
+
+    reduced_embedding = colapse_into_10(word_embedding)
+        
+    midi = np.zeros(10)
+    for dimension in range(10):
+        midi[dimension] = find_range(reduced_embedding[dimension], mappings[dimension])
+    return midi
